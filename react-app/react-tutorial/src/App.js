4b63c535337a1c9134698914bb8d03740d696f74
KimJinYeong <handayoung11@naver.com>
2020-11-07 00:17:34 +0900

    React Study

--- react-app/react-tutorial/src/App.js
+++ react-app/react-tutorial/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Contents from "./components/Content";
+import TOC from "./components/TOC";
+import Subject from "./components/Subject";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Subject title="WEB" sub="world wide web!" />
+				<Subject title="React" sub="For UI!" />
+				<TOC />
+				<Contents
+					title="HTML"
+					desc="HTML is HyperText Markup Language."
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
