cb11d9d2f31a64d90badc040d4fdb311b3a7d475
JinYeongKim <handayoung11@naver.com>
2020-12-21 23:28:57 +0900

    Vue Cart 기능 다듬기

--- Vue/Vue_Practice/0. Tutorial/main.js
+++ Vue/Vue_Practice/0. Tutorial/main.js
@@ -20,18 +20,23 @@ Vue.component("product", {
                  @mouseover="updateProduct(index)">
                 </div>
 
-                <button @click="addToCart(1)" 
+                <button @click="addToCart(true)" 
                         :disabled="!inStock"
-                        :class="{ disabledButton: !inStock }">Add to Cart</button>
-                <div class="cart">
-                    <p>Cart({{cart}})</p>
-                </div>
+						:class="{ disabledButton: !inStock }">Add to Cart</button>
+				<button @click="addToCart(false)" 
+						v-if="onCart != 0 && onCart != undefined"
+						class="delete"
+                        :class="{ disabledButton: !inStock }">Remove from Cart</button>
             </div>
         </div>
 	`,
 	props: {
 		premium: {
 			type: Boolean,
+			required: true,
+		},
+		cart: {
+			type: Array,
 			required: true,
 		},
 	},
@@ -52,21 +57,36 @@ Vue.component("product", {
 					variantId: 2235,
 					variantColor: "blue",
 					variantImage: "./assets/vmSocks-blue.jpg",
-					variantQuantity: 0,
+					variantQuantity: 3,
 				},
 			],
-			cart: 0,
 		};
 	},
 	methods: {
-		addToCart(count) {
-			this.cart = Math.max(this.cart + count, 0);
+		addToCart(add) {
+			// 현재 선택된 상품
+			const variant = this.variants[this.selectedVariant];
+
+			// 재고 없을 경우 장바구니 추가 불가능
+			if (this.onCart == variant.variantQuantity && add) {
+				alert("재고가 부족하여 더 이상 장바구니에 담을 수 없습니다");
+				return;
+			}
+
+			// 장바구니에 추가하기
+			this.$emit("add-to-cart", variant.variantId, add);
 		},
 		updateProduct(index) {
 			this.selectedVariant = index;
 		},
 	},
 	computed: {
+		onCart() {
+			// Cart에 있는 상품 개수
+			return this.cart.filter(
+				(word) => word == this.variants[this.selectedVariant].variantId
+			).length;
+		},
 		title() {
 			return this.brand + " " + this.product;
 		},
@@ -81,6 +101,21 @@ Vue.component("product", {
 const app = new Vue({
 	el: "#app",
 	data: {
-		premium: false,
+		cart: [],
+		premium: true,
+	},
+	methods: {
+		updateCart(id, add) {
+			//add 모드일 경우 장바구니에 추가
+			if (add) this.cart.push(id);
+			// 아닐 경우 장바구니에서 삭제
+			else {
+				for (let i = 0; i < this.cart.length; i++)
+					if (this.cart[i] == id) {
+						this.cart.splice(i, 1);
+						break;
+					}
+			}
+		},
 	},
 });
