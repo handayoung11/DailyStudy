08cb7fe91cb922a73e8d02f7cbc54464fe4b4dbd
JinYeongKim <handayoung11@gmail.com>
2021-02-15 00:20:01 +0900

    Lombok 설정 추가 및 VO 수정

--- Spring Summary/jpa-basic/src/main/java/hellojpa/Member.java
+++ Spring Summary/jpa-basic/src/main/java/hellojpa/Member.java
@@ -1,38 +1,35 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity //꼭 넣어야 된다. 이 Entity Annotation이 붙어있어야 JPA가 관리한다.
 @Table(name = "MEMBER") //Table명과 클래스명이 같으면 생략이 가능하다.
 public class Member {
     @Id //PK라는 것을 명시해주는 Annotation; 필수
     private Long id;
-    private String name;
+    @Column(name = "name", nullable = false)
+    private String username;
+    private String department;
+    private Integer age;
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob
+    private Integer description;
 
     public Member(Long id, String name) {
         this.id = id;
-        this.name = name;
-    }
-
-    protected Member() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+        this.username = name;
     }
 }
