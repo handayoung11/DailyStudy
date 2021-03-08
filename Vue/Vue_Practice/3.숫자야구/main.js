6b064d13b3806cfd06c5bfedc75fc9610c244633
JinYeongKim <handayoung11@gmail.com>
2021-03-08 22:00:07 +0900

    주석 수정

--- Vue/Vue_Practice/3.숫자야구/main.js
+++ Vue/Vue_Practice/3.숫자야구/main.js
@@ -1,4 +1,7 @@
-import Vue from "vue"; //ES6 문법 -> 설치한 모듈을 import를 통해 가져올 수 있다.
+// import문법: 기존에 ES2015에 들어있는 모듈시스템 문법. 설치한 vue를 가져오는 것이다.
+// 여기가 시작점이기때문에 어플리케이션에서 사용할 js들을 전부 여기로 땡겨와야 한다.
+import Vue from 'vue';
+
 import NumberBaseball from "./NumberBaseball";
 
-new Vue(NumberBaseball).$mount("#root"); //el 역할
+new Vue(NumberBaseball).$mount('#root');
