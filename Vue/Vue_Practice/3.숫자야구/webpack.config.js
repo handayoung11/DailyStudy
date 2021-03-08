43209c221d0f4c279a8ef7fb812b8c592b322fb8
JinYeongKim <handayoung11@gmail.com>
2021-03-08 22:01:10 +0900

    webpack 설정

--- Vue/Vue_Practice/3.숫자야구/webpack.config.js
+++ Vue/Vue_Practice/3.숫자야구/webpack.config.js
@@ -1,25 +1,32 @@
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const path = require('path'); // 현재 경로를 얻어오는 코드
+const path = require('path');
+//node의 모듈을 만든다. webpacking할 때 사용된다.
 module.exports = {
-  mode: 'development',
+    mode: 'development',
     devtool: 'eval',
     resolve: {
-      extensions: ['.js', '.vue'],
+        extensions: ['.js', '.vue'],
     },
-  entry: {
-    app: './main.js',
-  },
-  module: {
-      rules: [{
-          test: /\.vue$/,
-          loader: 'vue-loader',
-      }],
-  },
-  plugins: [
-      new VueLoaderPlugin(),
-  ],
-  output: {
-      filename: 'app.js',
-      path: path.join(__dirname, 'dist'),
-  },
+    entry: {
+        //가장 대표되는 js 파일
+        app: './main.js',
+    },
+    module: {
+        rules:[{
+         test:/\.vue$/,
+            loader: 'vue-loader',
+        }]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
+    output: {
+        //출력될 파일 이름
+        //[name].js도 가능 [name]은 entry의 app을 가져와서 지정한다.
+        filename: 'app.js',
+        path: path.join(__dirname, 'dist'),
+        /*
+        최종적으로 dist/app.js에 모든 script파일들이 합쳐진다.
+         */
+    },
 };
