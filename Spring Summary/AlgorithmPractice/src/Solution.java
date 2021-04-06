69d8003e95a973fcfc8ee497a82b8d86f9a93b2b
JinYeongKim <handayoung11@gmail.com>
2021-04-06 23:44:24 +0900

    Programmers: 정렬 > K번째 수 풀이

--- Spring Summary/AlgorithmPractice/src/Solution.java
+++ Spring Summary/AlgorithmPractice/src/Solution.java
@@ -1,26 +1,27 @@
-public class Solution {
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int time = 0;
-        int truckCount = truck_weights.length;
-        int truckStartTime[] = new int[truckCount];
-        int crossedTruck = 0;
-        int onBridge = 0;
-        int idx = -1;
-        while(crossedTruck != truckCount) {
-            if(idx < truckCount - 1 && weight >= onBridge + truck_weights[idx + 1]) {
-                idx += 1;
-                onBridge += truck_weights[idx];
-                truckStartTime[idx] = time;
-            }
-            time++;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-            for(int i = crossedTruck; i <= idx; i++) {
-                if(time - truckStartTime[i] == bridge_length) {
-                    onBridge -= truck_weights[i];
-                    crossedTruck += 1;
-                }
-            }
+public class Solution {
+//    정렬 K번째 수
+//    https://programmers.co.kr/learn/courses/30/lessons/42748?language=java
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        List<Integer> list = Arrays.stream(array).boxed().collect(Collectors.toList());
+
+        int i = 0;
+        for(int[] c : commands) {
+            List<Integer> cutList = list.subList(c[0] - 1, c[1]);
+            List<Integer> sortedList = cutList.stream().sorted().collect(Collectors.toList());
+            answer[i] = sortedList.get(c[2] - 1);
+//            System.out.println("sortedList = " + sortedList);
+            i++;
         }
-        return ++time;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solution(new int[] {1,5,2,6,3,7,4}, new int[][]{{2,5,3}, {4,4,1}, {1,7,3}});
     }
 }
