630eb64757f161f55bc7a7f9bbce739cb8f38193
JinYeongKim <handayoung11@gmail.com>
2021-04-07 23:39:53 +0900

    programmers: 정렬 > 가장 큰 수 코드 압축

--- Spring Summary/AlgorithmPractice/src/Solution.java
+++ Spring Summary/AlgorithmPractice/src/Solution.java
@@ -1,30 +1,19 @@
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class Solution {
 //    정렬 가장 큰 수
 //    https://programmers.co.kr/learn/courses/30/lessons/42746
 
-    class StrComparator implements Comparator<String> {
-        @Override
-        public int compare(String o1, String o2) {
-            int sum1 = Integer.parseInt(o1 + o2);
-            int sum2 = Integer.parseInt(o2 + o1);
-            if(sum1 > sum2) return 1;
-            if(sum1 == sum2) return 0;
-            else return -1;
-        }
-    }
-
     public String solution(int[] numbers) {
         StringBuilder answer = new StringBuilder();
-        List<String> list = Arrays.stream(numbers).mapToObj(i -> i + "").sorted(new StrComparator()).collect(Collectors.toList());
-        Collections.reverse(list);
-        list.forEach(n -> answer.append(n));
-        if(answer.substring(0, 1).equals("0")) return "0";
-        return answer.toString();
+        Arrays.stream(numbers).mapToObj(i -> i + "")
+                .sorted((o1, o2) -> {
+                    int sum1 = Integer.parseInt(o1 + o2);
+                    int sum2 = Integer.parseInt(o2 + o1);
+                    return sum1 > sum2?-1:sum1==sum2?0:1;
+                }).collect(Collectors.toList())
+                .forEach(n -> answer.append(n));
+        return answer.charAt(0) == '0'?"0":answer.toString();
     }
 }
