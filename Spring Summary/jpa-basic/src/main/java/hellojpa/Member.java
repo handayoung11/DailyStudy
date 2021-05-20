10540641c8237f4ef26efd68a3e53f25ccb68636
JinYeongKim <handayoung11@gmail.com>
2021-05-20 23:42:35 +0900

    Product Entity 생성 Member와 Product 매핑을 위한 MemberProduct Entity 생성 Member Entity와 Product Entity 다 대 다 양방향 연관관계 매핑

--- Spring Summary/jpa-basic/src/main/java/hellojpa/Member.java
+++ Spring Summary/jpa-basic/src/main/java/hellojpa/Member.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -28,4 +30,6 @@ public class Member {
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
 
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> products = new ArrayList<>();
 }
---
+++ Spring Summary/jpa-basic/src/main/java/hellojpa/Product.java
@@ -0,0 +1,27 @@
+package hellojpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "product")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
+
+    private int count;
+    private int price;
+    private LocalDateTime orderDateTime;
+}
